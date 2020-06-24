889d713709fa116a4e405d31e6be6ab238346f18
321910306025 <67199725+321910306025@users.noreply.github.com>
2020-06-24 20:02:30 +0530

    Update Assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,4 +1,3 @@
-
 1. Python program to take two numbers and add two numbers.
 Program:
 num1 = input('Enter first number: ')
@@ -10,6 +9,7 @@ Enter first number: 2
 Enter second number: 3
 The sum of 2.0 and 3.0 is 5.0
 
+
 2. Python program that uses input to prompt a user for their name and then welcomes them.
 Program:
 name = input('Enter your name: ')
@@ -17,6 +17,7 @@ print("Welcome", name)
 Output:
 Enter your name: Heena
 Welcome Heena
+
 
 3. Python program to prompt the user for hours and rate per hour to compute gross pay.
 Program:
@@ -28,6 +29,7 @@ Output:
 Enter hour: 35
 Enter rate per hour:2.75
 Pay: 96.25
+
 
 4.Assume that we execute the following assignment statements:
 width = 1
@@ -55,3 +57,4 @@ print('%.2f celsius is: %0.2f fahrenheit' %(celsius, fahrenheit))
 Output:
 Enter temperature in celcius: 37
 37.00 celcius is: 98.60 fahrenheit
+
