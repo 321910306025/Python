3fbcf76e721fb573099f7628490dbce8399c6731
321910306025 <67199725+321910306025@users.noreply.github.com>
2020-07-02 16:37:21 +0530

    Create Assignment5_files&directories.py

---
+++ Assignment5_files&directories.py
@@ -0,0 +1,10 @@
+1. Python program to open a file using try and except.
+Program:
+try:
+    f = open("test.txt", "r")
+except IOError:
+    print "Oops, no file by that name"
+
+
+2. Python program to split the data in a file. 
+Program:
